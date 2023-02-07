fdac05e4e957faa55dad79c18e8fffdd7db2d1a6
alexquu <alexqu86@gmail.com>
2023-02-07 17:52:59 +0100

    URL server fixing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {HashRouter as Router, Route, Routes} from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import News from "./pages/News";
 import Gigs from "./pages/Gigs";
@@ -8,18 +8,17 @@ import Contact from "./pages/Contact";
 import "./fonts/Homade.otf";
 
 function App() {
-  
   return (
-    <Router>
-      <Routes>
+      <Router>
+        <Routes>
         <Route path="/" element={<Homepage/>}/>
         <Route path="/News" element={<News />}/>
         <Route path="/Gigs" element={<Gigs />}/>
         <Route path="/Bio" element={<Bio/>}/>
         <Route path="/Media" element={<Media/>}/>
         <Route path="/Contact" element={<Contact/>}/>
-      </Routes>
-    </Router>
+        </Routes>
+      </Router>
   );
 }
 
